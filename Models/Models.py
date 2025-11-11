bceab9cb5c0e1aba733e7c044314665bfcd2da06
João Pontes <136093514+Joao-Pontes22@users.noreply.github.com>
2025-11-11 15:09:11 -0300

    Update Models.py

--- Models/Models.py
+++ Models/Models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine, Column, Date,  Integer, String, Integer, Float,ForeignKey
 from sqlalchemy.orm import declarative_base, relationship
 
-db = create_engine("postgresql://postgres:pgadmin@localhost:5432/Finances"
+db = create_engine("postgresql+psycopg2://finances_v7j8_user:5OqAIRw61E0lkEjF4aEitwwBpQyOwDoq@dpg-d49kqcq4d50c739cu690-a.virginia-postgres.render.com:5432/finances_v7j8?sslmode=require"
 )
 base = declarative_base()
 
