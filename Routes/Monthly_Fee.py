f270c28c5692e2ccf542e85e831f5f0d8fa0aeff
João Pontes <joao13789@gmail.com>
2025-11-12 12:14:57 -0300

    Assistente-financeiro V03

--- Routes/Monthly_Fee.py
+++ Routes/Monthly_Fee.py
@@ -27,19 +27,24 @@ async def Update_Monthly_Fee(id:int, scheme: Update_Monthly_Fee_Scheme, session:
     monthly_fee.Category = scheme.Category
     monthly_fee.Monthly_Value = scheme.Monthly_Value
     monthly_fee.Status = scheme.Status
-
+    session.commit()
     return{"message": "Mensalidade atualizada com sucesso",
            "Dados": monthly_fee}
 
 @Monthly_Fee_Router.put("/Update_Greater_than_month_Monthly_Fee")
-async def Update_Monthly_Fee(month:int, scheme: Update_Monthly_Fee_Scheme, session:Session = Depends(init_session)):
-    monthly_fee = session.query(Monthly_Fee).filter(extract("month",Monthly_Fee.Date) >= month).all()
+async def Update_Monthly_Fee(year:int, month:int, scheme: Update_Monthly_Fee_Scheme, session:Session = Depends(init_session)):
+    monthly_fee = session.query(Monthly_Fee).filter(extract("year",Monthly_Fee.Date) >= year,extract("month",Monthly_Fee.Date) >= month).all()
     for i in monthly_fee:
         i.Description = scheme.Description
         i.Date = scheme.Date
         i.Category = scheme.Category
         i.Monthly_Value = scheme.Monthly_Value
         i.Status = scheme.Status
-
+    session.commit()
     return{"message": "Mensalidade atualizada com sucesso",
            "Dados": monthly_fee}
+
+@Monthly_Fee_Router.get("/Update_Greater_than_month_Monthly_Fee")
+async def Update_Monthly_Fee(year: int,month:int, session:Session = Depends(init_session)):
+    monthly_fee = session.query(Monthly_Fee).filter(extract("year",Monthly_Fee.Date) >= year,extract("month",Monthly_Fee.Date) >= month).all()
+    return monthly_fee
