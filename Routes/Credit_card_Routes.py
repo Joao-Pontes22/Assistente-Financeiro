8f6ac1b672ae57c2fddbcfd889ab6a4fd47fb52c
João Pontes <136093514+Joao-Pontes22@users.noreply.github.com>
2025-11-17 20:09:26 -0300

    Update Credit_card_Routes.py4

--- Routes/Credit_card_Routes.py
+++ Routes/Credit_card_Routes.py
@@ -291,7 +291,7 @@ async def View_expenses(month:int = None,
                         extract('year', Credit_card.Date):  year,
                         extract('month', Credit_card.Date): month}
     expenses = None
-     mf = []
+    mf = []
     for column, value in credit_card_array.items():
         if  value is  not None:
             expenses = session.query(Credit_card).filter(column == value).all()
