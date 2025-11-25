ab3ed7060c859c727a51e7bfb3bc260a8c677c2f
João Pontes <136093514+Joao-Pontes22@users.noreply.github.com>
2025-11-25 12:32:04 -0300

    Update DATABASE_URL for database connection

--- Models/Models.py
+++ Models/Models.py
@@ -4,7 +4,7 @@ import os
 
 DATABASE_URL = os.getenv(
     "DATABASE_URL_INTERNAL",
-    "postgresql+psycopg2://finances_v7j8_user:5OqAIRw61E0lkEjF4aEitwwBpQyOwDoq@dpg-d49kqcq4d50c739cu690-a.virginia-postgres.render.com:5432/finances_v7j8?sslmode=require"
+    "postgresql://projetos_qkuw_user:mHpA4su1PJuZ0DgfSzkGnISABy0l4LXN@dpg-d4iqc1s9c44c73b2dgtg-a/projetos_qkuw"
 )
 
 db = create_engine(DATABASE_URL)
